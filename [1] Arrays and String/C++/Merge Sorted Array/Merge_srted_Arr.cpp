feeccebab57d5bca8f2f982da532c90fd8eff4b9
Muhammad Umar <umar@DESKTOP-9BESRAR.localdomain>
2026-02-24 17:36:19 +0800

    Beginning Merge Sort Array Problem, Writing initial Algo

--- [1] Arrays and String/C++/Merge Sorted Array/Merge_srted_Arr.cpp
+++ [1] Arrays and String/C++/Merge Sorted Array/Merge_srted_Arr.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+//Writing "vector<int>&nums 1" Allows this vector to be modified when it is called in the main loop. No copy of this function is made, which means that this function will
+//apply changes to real Data of an array when called.
+//
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    return; 
+    }
 
+int countNonZero(const vector<int> a){
+    int count = 0;
+    for (int num : a ){
+        if (num != 0){
+            count++;
+        }
+    }
+    return count;
+}
+
+int main(){
+    vector <int> nums1 = {1,2,3,0,0,0,0};
+    vector <int> nums2 = {2,5,6};
+    int L_n1=countNonZero(nums1);
+    cout<<"The number of elements in Array 1 :"<<L_n1;
+    return 0;
+
+}
