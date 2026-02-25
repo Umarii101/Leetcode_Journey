52999ecb52aa1317a9de69adb93de4b5bd6ef771
Muhammad Umar <umar@DESKTOP-9BESRAR.localdomain>
2026-02-25 17:06:12 +0800

    Merge Sorted Arrays Solved

--- [1] Arrays and String/C++/Merge Sorted Array/Merge_srted_Arr.cpp
+++ [1] Arrays and String/C++/Merge Sorted Array/Merge_srted_Arr.cpp
@@ -5,8 +5,19 @@ using namespace std;
 //apply changes to real Data of an array when called.
 //
 void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    return; 
-    }
+    int last = m + n -1; //Last Index
+    while(m>0 && n>0){
+        if (nums1[m-1] > nums2[n-1]){
+            nums1[last]= nums1[m-1];
+            m -= 1;
+        }
+        else{
+            nums1[last] = nums2[n-1];
+            n -= 1;
+        }
+        last -=1;
+    }   
+}
 
 int countNonZero(const vector<int> a){
     int count = 0;
@@ -21,8 +32,50 @@ int countNonZero(const vector<int> a){
 int main(){
     vector <int> nums1 = {1,2,3,0,0,0,0};
     vector <int> nums2 = {2,5,6};
-    int L_n1=countNonZero(nums1);
-    cout<<"The number of elements in Array 1 :"<<L_n1;
+    int m=countNonZero(nums1);
+    int n=countNonZero(nums2);
+    cout<<"Input nums1 = [";
+    for(int i=0;i<m+n;i++){
+        cout<<nums1[i];
+        }
+    cout<<"]\n";
+    cout<<"Input nums2 = [";
+    for(int i=0;i<n;i++){
+        cout<<nums1[i];
+        }
+    cout<<"]\n";
+
+
+    merge(nums1,m,nums2,n);
+    cout<<"nums1 after the Merge = [";
+    for(int i=0;i<m+n;i++){
+        cout<<nums1[i];
+        }
+    cout<<"]\n";
     return 0;
 
 }
+
+//Example 1:
+
+//Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//Output: [1,2,2,3,5,6]
+//Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+//The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+
+//Example 2:
+
+//Input: nums1 = [1], m = 1, nums2 = [], n = 0
+//Output: [1]
+//Explanation: The arrays we are merging are [1] and [].
+//The result of the merge is [1]
+
+
+//Example 3:
+
+//Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+//Output: [1]
+//Explanation: The arrays we are merging are [] and [1].
+//The result of the merge is [1].
+//Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
