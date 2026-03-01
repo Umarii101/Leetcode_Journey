480eb929214c79a07455083cdba3f4232313c83e
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-01 17:54:47 +0800

    Solved the time constraint aswell.

--- [1] Arrays and String/C++/Remove Duplicates from Sorted Array/Rmv_Dups_frm_Srtd_Arrs.cpp
+++ [1] Arrays and String/C++/Remove Duplicates from Sorted Array/Rmv_Dups_frm_Srtd_Arrs.cpp
@@ -1,37 +1,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int removeDuplicates(vector<int>&nums){
-    int last = nums.size(); //Last index of The input Array.
-    last -=1;
-    cout<<"=========================================="<<endl;
-    cout<<"====== Operation Duplicates Removal ======="<<endl;
-    cout<<"=========================================="<<endl;
-    cout<<"\nThe Last index in the input Array = :"<<last<<endl<<endl;
-    for(int i = 0;i<last;i++){
-        if(nums[i]==nums[i+1]){
-            cout<<endl;
-            cout<<"Found the duplicate at Indexes : ["<<i<<", "<<i+1<<"] with value ("<<nums[i]<<", "<<nums[i+1]<<")"<<endl;
-            cout<<endl;
-            //When a duplicate is found, I need to left shift the whole array until last index, and place a 0 in place of last index.
-            int j=i+1; //We want index i at its own place, we will shift from next indexes.
-            while(j<last){
-                cout<<"Replacing index value ("<<j<<", "<<nums[j]<<") with ("<<j+1<<", "<<nums[j+1]<<")"<<endl;
-                nums[j]=nums[j+1];//Up until this point, the function can handle only 2 duplicates.
-                j++;
-                
-            }
-            nums[last] = 0;
-            last -=1;
-            //Placing 0 at last index, and moving the Last index pointer to the left one time.
-            //Need an additional check to make sure that the duplicates are repeated or not in the next indexes.
+int removeDuplicates(vector<int>& nums) {
+        int last = nums.size() - 1;
+        for(int i = 0; i < last; i++){
             if(nums[i] == nums[i+1]){
-                i -=1; //So the pointer ends up at i again when this condition is met.
+                int j = i+1;
+                while(j < last){
+                    nums[j] = nums[j+1];
+                    j++;
+                }
+                nums[last] = 0;
+                last--;
+                i--;  // Recheck this position
             }
         }
-
-    }
-    return last+1;
+        return last + 1;
 }
 int main(){
     vector <int> nums = {1,1,2,2,5,5,7,7,7,8};
