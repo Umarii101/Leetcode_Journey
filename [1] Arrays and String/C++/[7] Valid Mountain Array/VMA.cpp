9d23035e9416ff6e58a4f24ead0babcab27217c2
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-03 22:55:07 +0800

    Solving A descending Slope bug in problem [7]

--- [1] Arrays and String/C++/[7] Valid Mountain Array/VMA.cpp
+++ [1] Arrays and String/C++/[7] Valid Mountain Array/VMA.cpp
@@ -4,12 +4,19 @@ using namespace std;
 
 bool validMountainArray(vector<int>& arr) {
     if(arr.size()>=3){
+        bool stat=0;
         for(int i=1;i<arr.size();i++){
             if(arr[i]>arr[i-1]){
+                bool stat = 1;//To keep a record of actually having an upward direction initially in the array.
                 continue;
             }
             else{
+                if(stat==0){
+                    cout<<"The array is just a descending downhill direction\n";
+                    return 0;
+                }
                 while(i<arr.size()){
+
                     if(arr[i]<arr[i-1]){
                         i++;
                         cout<<i<<endl;
@@ -19,7 +26,7 @@ bool validMountainArray(vector<int>& arr) {
                         cout<<"Not a strict mountain at i = ["<<i<<"]\n";
                         return 0;
                     }
-                } return 1;
+                }
             }
        }
     }
@@ -32,7 +39,7 @@ bool validMountainArray(vector<int>& arr) {
 }
 
 int main(){
-    vector<int> arr = {0,1,2,3,4,5,6,7,8,9,10};
+    vector<int> arr = {9,8,7,6,5,4,3,2,1,0};
     bool sol = validMountainArray(arr);
     if (sol){
         cout<<"This is a Valid Mountain Array\n";
