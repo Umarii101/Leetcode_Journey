55be04cd7a01102b7119b1c2202a1ca01fb2a96d
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-03 17:50:35 +0800

    Starting Valid Mountain Arrays Problem

---
+++ [1] Arrays and String/C++/[7] Valid Mountain Array/VMA.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool validMountainArray(vector<int>& arr) {
+        
+}
+
+int main(){
+    vector<int> arr = {0,3,2,1};
+    return 0;
+}
