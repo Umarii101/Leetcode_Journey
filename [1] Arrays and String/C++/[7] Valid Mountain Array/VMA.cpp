0e5f2381eb054d83246d54de1bad83d89b0ec1fe
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-03 23:02:47 +0800

    Adding Flag to verify upward initialization of Array

--- [1] Arrays and String/C++/[7] Valid Mountain Array/VMA.cpp
+++ [1] Arrays and String/C++/[7] Valid Mountain Array/VMA.cpp
@@ -7,11 +7,16 @@ bool validMountainArray(vector<int>& arr) {
         bool stat=0;
         for(int i=1;i<arr.size();i++){
             if(arr[i]>arr[i-1]){
-                bool stat = 1;//To keep a record of actually having an upward direction initially in the array.
+                cout<<"The Array starts in upward Direction [Good]\n";
+                stat = 1;//To keep a record of actually having an upward direction initially in the array.
+                cout<<"[Write] Status of our Bool = ["<<stat<<"]\n";
                 continue;
             }
             else{
+                cout<<"[Read 0] Status of our Bool = ["<<stat<<"]\n";
+
                 if(stat==0){
+                    cout<<"[Read 1] Status of our Bool = ["<<stat<<"]\n";
                     cout<<"The array is just a descending downhill direction\n";
                     return 0;
                 }
@@ -26,7 +31,7 @@ bool validMountainArray(vector<int>& arr) {
                         cout<<"Not a strict mountain at i = ["<<i<<"]\n";
                         return 0;
                     }
-                }
+                } return 1;
             }
        }
     }
@@ -39,7 +44,7 @@ bool validMountainArray(vector<int>& arr) {
 }
 
 int main(){
-    vector<int> arr = {9,8,7,6,5,4,3,2,1,0};
+    vector<int> arr = {0,3,2,1};
     bool sol = validMountainArray(arr);
     if (sol){
         cout<<"This is a Valid Mountain Array\n";
