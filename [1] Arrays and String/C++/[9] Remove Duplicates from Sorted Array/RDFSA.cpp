bf3c212bab998c3274f993e82e4c09488e04d9f1
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-09 21:30:19 +0800

    2 Duplicates removal Done, 2+ with bugs, Resolving

--- [1] Arrays and String/C++/[9] Remove Duplicates from Sorted Array/RDFSA.cpp
+++ [1] Arrays and String/C++/[9] Remove Duplicates from Sorted Array/RDFSA.cpp
@@ -4,24 +4,23 @@ using namespace std;
 
 int removeDuplicates(vector<int>& nums) {
     int length = nums.size()-1;
-    int dup_ptr =0; //This pointer will point to the latest index where a duplicate was found at. 
-    //Length becomes 2 here, 0-2.
     for(int i =0; i<length;i++){
-        for(int j=i+1;j<length+1;j++){
-            if(nums[i] == nums[j]){
-                cout<<"For (index,Value) = "<<i<<", "<<nums[i]<<"\t\tFound a duplicate at j = "<<j<<endl;
-                cout<<"Replacing Index j = "<<j<<" with "<<nums[j+1]<<"\n\n";
-                dup_ptr=j;
+        int j= i+1;
+        if(nums[i] == nums[j]){
+            cout<<"For (index,Value) = "<<i<<", "<<nums[i]<<"\t\tFound a duplicate at j = "<<j<<endl;
+            cout<<"Replacing Index j = "<<j<<" with "<<nums[j+1]<<"\n\n";
+            while(j<length){
                 nums[j]=nums[j+1];
                 nums[j+1]=0;
-            }
+                j++;
+                }
        }
     }
     return length;
 }
 
 int main(){
-    vector <int> nums = {1,1,2};
+    vector <int> nums = {0,0,1,1,1,2,2,3,3,4};
     //Expected Output = 2, {1,2,_}; (Does'nt matter what we leave behind, hence Underscores
     int length = removeDuplicates(nums);
     cout<<"Output Array = [";
