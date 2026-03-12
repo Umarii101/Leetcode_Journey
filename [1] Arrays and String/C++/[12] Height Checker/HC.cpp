dc4d65bee743c08092ea89407eb89004651062ec
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-12 22:27:58 +0800

    Perfectly nailed the solution, But it has the worst time complexity and Memory usage lol

--- [1] Arrays and String/C++/[12] Height Checker/HC.cpp
+++ [1] Arrays and String/C++/[12] Height Checker/HC.cpp
@@ -15,11 +15,22 @@ int heightChecker(vector<int>& heights) {
             nums[i]=nums[i+1];
             nums[i+1]=temp;
             temp=0;
-            i = 0; //Recheck indice.
+            i = -1; //Recheck indice.
         }
     }
     cout<<"Done Sorting \nPurging --> Misplaced Indices\n";
-    for(int i =0;i<heights.size();i++){
+    cout<<"\n\n\nInput Array = [";
+    for(int i=0;i<heights.size();i++){
+        cout<<heights[i]<<", ";
+    }
+    cout<<"]"<<endl;
+    cout<<"\nSorted Array = [";
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<", ";
+    }
+    cout<<"]\n\n"<<endl;
+    
+   for(int i =0;i<heights.size();i++){
         if(heights[i] != nums[i]){
             cout<<"Misplaced at i =  "<<i<<endl;
             count++;
@@ -29,15 +40,8 @@ int heightChecker(vector<int>& heights) {
 }
 
 int main(){
-    vector <int> heights = {1,1,4,2,1,3};
-    //Expected Output = {1,1,1,2,3,4}; Arranged in Order and Also returning the incorrect indices, Such as (2,4,5).
+    vector <int> heights = {9,2,8,3,8,4,6,6,8,3,3,1,2,8,9,6,6,5,3,2};
     int output = heightChecker(heights);
-    cout<<"\n[";
-    for(int i=0;i<heights.size();i++){
-        cout<<heights[i]<<", ";
-    }
-    cout<<"]"<<endl;
-
     cout<<"\nThe Total Misplaced Indices are Total = "<<output<<endl;
     return 0;
 }
