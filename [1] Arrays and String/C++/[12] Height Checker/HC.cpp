d5f9a0923b569719e75f3fde5ae62f7cd156f18d
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-12 22:20:46 +0800

    Updated Algorithm, Now we first Sort the Array in a copy and then Compare to find the misplaced indices

--- [1] Arrays and String/C++/[12] Height Checker/HC.cpp
+++ [1] Arrays and String/C++/[12] Height Checker/HC.cpp
@@ -4,18 +4,25 @@ using namespace std;
 
 int heightChecker(vector<int>& heights) {
     int count =0;
-    for(int i =0; i<heights.size();i++){
+    //First Create Another Sorted Array and then Compare.
+    vector <int> nums = heights;
+    int wp =0; //Write pointer;
+    for(int i=0; i<nums.size()-1;i++){
         cout<<"i = "<<i<<endl;
-        if(heights[i]>heights[i+1]){
-            cout<<"\t\tFound an incorrect indice at i = "<<i<<endl;
-            count++;
+        int temp = 0;
+        if (nums[i]>nums[i+1]){
+            temp = nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=temp;
+            temp=0;
+            i = 0; //Recheck indice.
         }
-        if(i==heights.size()-1){
-            cout<<"Reached the final Indice i = "<<i<<endl;
-            if(heights[i]>heights[i-1]){
-                cout<<"\t\tFound an incorrect indice at i = "<<i<<endl;
-                count++;
-            }
+    }
+    cout<<"Done Sorting \nPurging --> Misplaced Indices\n";
+    for(int i =0;i<heights.size();i++){
+        if(heights[i] != nums[i]){
+            cout<<"Misplaced at i =  "<<i<<endl;
+            count++;
         }
     }
     return count;
@@ -25,6 +32,12 @@ int main(){
     vector <int> heights = {1,1,4,2,1,3};
     //Expected Output = {1,1,1,2,3,4}; Arranged in Order and Also returning the incorrect indices, Such as (2,4,5).
     int output = heightChecker(heights);
+    cout<<"\n[";
+    for(int i=0;i<heights.size();i++){
+        cout<<heights[i]<<", ";
+    }
+    cout<<"]"<<endl;
+
     cout<<"\nThe Total Misplaced Indices are Total = "<<output<<endl;
     return 0;
 }
