83417e1caca980395806af4571bde85d2f08d6df
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-17 22:31:05 +0800

    Starting Problem [14]

---
+++ [1] Arrays and String/C++/[14] Find All Numbers Disappeared in an Array/fandia.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> findDisappearedNumbers(vector<int>& nums) {
+        
+}
+
+int main(){
+    vector<int> nums = {};
+    return 0;
+}
