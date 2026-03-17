63d4e77c1e2b8c4066d99aed5d203ebde7c8b58b
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-17 22:44:50 +0800

    Built Core logic to find Max number in the Array first to compare the Disappeared Numbers

--- [1] Arrays and String/C++/[14] Find All Numbers Disappeared in an Array/fandia.cpp
+++ [1] Arrays and String/C++/[14] Find All Numbers Disappeared in an Array/fandia.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using namespace std;
+//using namespace std;
 
-vector<int> findDisappearedNumbers(vector<int>& nums) {
-        
+std::vector<int> findDisappearedNumbers(std::vector<int>& nums) {
+    //Find max Range of (1,n);
+    int max = *std::max_element(nums.begin(), nums.end());
+    std::cout << "Max value: " << max << std::endl;
+    return nums;
 }
 
 int main(){
-    vector<int> nums = {};
+    std::vector<int> nums = {4,3,2,7,8,2,3,1};
+    //Expected Output = {5,6}, Since they are not in the list of range (1,n).
+    std::vector<int> output = findDisappearedNumbers(nums);
     return 0;
 }
