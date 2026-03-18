7c0b0530b7b749a528ec53f1ad631b675602a722
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-18 22:02:01 +0800

    Solved provlem [14] with 50 Percentile memory efficiency but Time complexity is too much. Will Try another method to Solve time Complexity

--- [1] Arrays and String/C++/[14] Find All Numbers Disappeared in an Array/fandia.cpp
+++ [1] Arrays and String/C++/[14] Find All Numbers Disappeared in an Array/fandia.cpp
@@ -2,29 +2,19 @@
 #include <vector>
 #include <algorithm>
 
-//using namespace std;
-
 std::vector<int> findDisappearedNumbers(std::vector<int>& nums) {
-    //Find max Range of (1,n);
-    int max = *std::max_element(nums.begin(), nums.end());
-    std::cout << "Max value: " << max << std::endl;
-    //We've Found the maximun Value in the array, Now we need to compare only with O(n) loop.
+    //Defining the Resultant Array;
     std::vector<int> Disappeared_nums;
-    int j=0;
+
+    //Implementing Binary Search -> [1] Sorting Array first.
+    std::sort(nums.begin(),nums.end());
     
-    //Outer Loop to track all the elements of the nums Array.
-    for(int k=0; k<nums.size();k++){
-        //Inner Loop to compare a single index of Array with (1,max).
-        for(int i=1; i<=max;i++){
-            if(nums[k]==i){
-                std::cout<<"Value "<<i<<" Exists. Proceed \n";
-            }
-            else{
-                //Creating the Resultant Array with total j indexes.
-                std::cout<<"Found Missing Number "<<i<<"\n";
-                Disappeared_nums.push_back(i);
-                j++;
-            }
+    //[2] Now implementing Binary Search
+    for(int i=1;i<=nums.size();i++){
+        std::cout<<"i = "<<i<<"\n\n";
+        if(!std::binary_search(nums.begin(),nums.end(),i)){
+            std::cout<<"Found Disappeared Number --> ["<<i<<"]\n";
+            Disappeared_nums.push_back(i);
         }
     }
 
@@ -32,7 +22,7 @@ std::vector<int> findDisappearedNumbers(std::vector<int>& nums) {
 }
 
 int main(){
-    std::vector<int> nums = {4,3,2,7,8,2,3,1};
+    std::vector<int> nums = {1,1};
     //Expected Output = {5,6}, Since they are not in the list of range (1,n).
     std::vector<int> output = findDisappearedNumbers(nums);
     std::cout<<"\n\nOutput Array = [";
