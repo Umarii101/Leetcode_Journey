5754d44546318b379544528d39d9c87b12ba424d
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-23 20:09:36 +0800

    Designed the void put and int get Func()

--- [2] Hash Tables/[2] Design HashMap/HashMap.cpp
+++ [2] Hash Tables/[2] Design HashMap/HashMap.cpp
@@ -6,22 +6,43 @@
 class MyHashMap{
     private: 
         int numBuckets;
-        std::vector<std::list<int> buckets;
+        std::vector<std::list<std::pair<int,int>>> buckets;
 
         int hash(int key){
             return key % numBuckets;
         }
 
     public:
+
         MyHashMap(){
         numBuckets = 1007;
-        buckets = std::vector<std::list<int>>(numBuckets);
+        buckets.resize(numBuckets);
         }
 
         void put(int key, int value){    
-         int index = hash(key);
+            int index = hash(key);
+            // Look through the list at this bucket
+            for (auto &pair : buckets[index]) { //The '&' Ensures that when we find the key, the update happens in the Actual HashMap instead of a copy.
+                if (pair.first == key) {
+                    // Key exists! Update the value and exit
+                    pair.second = value;
+                    return;
+                }
+            }
 
+        // Key doesn't exist, so add a new pair to the list
+        buckets[index].push_back({key, value});        
         }
+        
+        int get(key){
+            int index = hash(key);
+            for(auto &pair : buckets[index]){
+                if(pair.first == key){
+                    return 1; //The key exists and we return True as 1.
+                }    
+            }
+            return -1; //The Key does not exist in this our Hashmap.
+    
 
 
 };
