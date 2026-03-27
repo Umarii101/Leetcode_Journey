88726eec4790391cee5088df60fac74cec0d508f
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-27 18:51:38 +0800

    Solved all bugs, Beats 90.59% in Memory Efficiency.

--- [2] Hash Tables/[5] Intersection of Two Arrays/Intersection_2Arrays.cpp
+++ [2] Hash Tables/[5] Intersection of Two Arrays/Intersection_2Arrays.cpp
@@ -4,77 +4,61 @@
 #include <list>
 #include <unordered_set>
 
-std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2){
+
+std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2) {
     std::vector<int> result;
     int size1 = nums1.size();
     int size2 = nums2.size();
 
-    //Calculating the total iterations we need. In my opinion it will be more efficient to only iterate all the elements of the smaller vector.
-    std::vector<int> total_iterations;
-    int difference = size1-size2;
-    if(difference > 0){
-        for(int num : nums2){
-            //Before Proceeding, It would be wise to check if The element already exists in the resultant array or not. This shall prevent duplicates in Result and keep it unique.
+    // Strategy: Iterate through the smaller array to be efficient
+    if (size1 > size2) {
+        for (int num : nums2) {
+            // Check if already in our result vector
             bool seen = false;
-            for(int j = 0; j <=result.size();j++){
-                if(num== nums2[j]){
-                    //Already Exists.
+            for (int j = 0; j < result.size(); j++) {
+                if (num == result[j]) {
                     seen = true;
                     break;
                 }
-                else{
-                    continue;
+            }
+
+            // If not seen, look for it in the larger array (nums1)
+            if (!seen) {
+                for (int i = 0; i < size1; i++) {
+                    if (num == nums1[i]) {
+                        result.push_back(num);
+                        break; // CRITICAL: Stop once found to stay unique
+                    }
                 }
             }
-            for(int i=0; i<size1;i++){
-                if(num == nums2[i] && !seen){//Iterate through all the Elements of The other array.
-                    //Found the element that is intersecting.
-                    result.push_back(num);
-                    i++;
+        }
+    } 
+
+    else {
+        for (int num : nums1) {
+            // Check if already in our result vector
+            bool seen = false;
+            for (int j = 0; j < result.size(); j++) {
+                if (num == result[j]) {
+                    seen = true;
+                    break;
                 }
-                else{
-                    //it does not match. Move on:
-                    i++;
+            }
+
+            // If not seen, look for it in the other array (nums2)
+            if (!seen) {
+                for (int i = 0; i < size2; i++) {
+                    if (num == nums2[i]) {
+                        result.push_back(num);
+                        break; // CRITICAL: Stop once found to stay unique
+                    }
                 }
             }
         }
     }
 
-    else{
-        for(int num : nums1){
-            bool seen = false;
-            for(int j = 0; j <=result.size();j++){
-                if(num== nums1[j]){
-                    //Already Exists.
-                    seen = true;
-                    break;
-                }
-                else{
-                    continue;
-                }
-            }
-            for(int i=0; i<size2;i++){
-                if(num == nums1[i] && !seen){//Iterate through all the Elements of The other array.
-                    //Found the element that is intersecting.
-                    result.push_back(num);
-                    i++;
-                }
-                else{
-                    //it does not match. Move on:
-                    i++;
-                }
-            }
-        }
-    }    
-
     return result;
 }
-
-//My Approach:
-//  
-
-
-
 
 
 int main(){
@@ -84,7 +68,7 @@ int main(){
     std::vector<int> Result = intersection(nums1,nums2);
     std::cout<<"The Total Elements in Result are : ["<<Result.size()<<"]\n";
     std::cout<<"\nResultant Vector = [";
-    for(int i =0; i < Result.size()-1;i++){
+    for(int i =0; i < Result.size();i++){
         std::cout<<Result[i];
     }
     std::cout<<"]\n";
