28dbdf43ee4fe7c4219cb3badc707825a870f5e5
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-27 18:03:14 +0800

    Started [5] Intersection of Two Arrays Problem

---
+++ [2] Hash Tables/[5] Intersection of Two Arrays/Intersection_2Arrays.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <unordered_set>
+std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2){
+    std::vector<int> result;
+    
+    return result;
+}
+
+//My Approach:
+//  
+
+
+
+
+
+int main(){
+    std::vector<int> nums1 = {1,2,2,1};
+    std::vector<int> nums2 = {2,2};
+    //Result = {2}. Only Unique intersection elements in the Resultant Array should be returned. The order does not matter.
+
+    return 0;
+}
