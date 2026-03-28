b9067919f7ecf071bf92e7a0b05142e2f32993d2
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-28 18:51:35 +0800

    Happy Number Solved. Will test with additional Test cases and submit to Leetcode Next

--- [2] Hash Tables/[6] Happy Number/Happy_Number.cpp
+++ [2] Hash Tables/[6] Happy Number/Happy_Number.cpp
@@ -9,18 +9,31 @@ bool isHappy(int n) {
         digits.push_back(n % 10); // Add the last digit
         n = n / 10;               // Remove the last digit
     }
-
     // Print results
     std::cout << "The total Digits in [n] = [";
     for(int j = 0; j < digits.size(); j++){
         std::cout << digits[j] << (j == digits.size() - 1 ? "" : ", ");
     }
     std::cout << "]\n";
-
-    return false; // Placeholder
+    int last = *(digits.end()-1);
+    std::cout<<"The Last Digit is : "<<last<<"\n";
+    if(last == 1){
+        return true; //Happy Number
+    }
+    else{
+        return false; // Not a happy Number
+    }
 }
 
 int main(){
-    isHappy(19); // Test with 19
+    bool Result;
+    int n = 181;
+    Result = isHappy(n);
+    if(Result){
+        std::cout<<n<<" is a Happy Number! :)\n";
+    }
+    else{
+        std::cout<<n<<" is not a Happy Number! :(\n";
+    }
     return 0;
 }
