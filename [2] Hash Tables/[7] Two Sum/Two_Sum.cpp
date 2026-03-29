92a8258995524a1bd8144a62207e603343591e8a
Muhammad Umar <sultanumar2002@gmail.com>
2026-03-29 16:51:10 +0800

    Started Problem [7] Two Sum

---
+++ [2] Hash Tables/[7] Two Sum/Two_Sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+std::vector<int> twoSum(std::vector<int> nums, int target){
+    std::vector<int> result = {1,2};   
+
+    std::cout<<"Function says Hello\n";
+
+    return result;
+}
+
+int main(){
+    std::vector<int> nums = {2,7,11,15};
+    int Target = 9;
+    std::vector<int> Result;
+    Result = twoSum(nums,Target);
+
+
+
+    return 0;
+}
