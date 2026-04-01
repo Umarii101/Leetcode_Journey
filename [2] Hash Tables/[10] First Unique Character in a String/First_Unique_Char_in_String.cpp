4eaaace88ee559d37db1a520b57320ca3c0fc12d
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-01 18:24:03 +0800

    Started Problem [10] First Unique Character in a string

---
+++ [2] Hash Tables/[10] First Unique Character in a String/First_Unique_Char_in_String.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+int firstUniqChar(std::string s) {
+
+
+
+
+}
+
+
+
+
+
+int main(){
+    std::string s = "leetcode";
+//Input: s = "leetcode"
+//Output: 0
+//Explanation: The character 'l' at index 0 is the first character that does not occur at any other index.
+
+    return 0;
+}
