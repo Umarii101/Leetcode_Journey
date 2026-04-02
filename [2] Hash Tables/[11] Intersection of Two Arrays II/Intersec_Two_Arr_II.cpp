f551b1ae701c7c199687fd90dc70c3854fb37d48
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-02 17:54:25 +0800

    Problem solved but is Highly inefficient since i have not used any concept of Hashing at all, I could not think of a hashing solution

--- [2] Hash Tables/[11] Intersection of Two Arrays II/Intersec_Two_Arr_II.cpp
+++ [2] Hash Tables/[11] Intersection of Two Arrays II/Intersec_Two_Arr_II.cpp
@@ -4,8 +4,6 @@
 
 std::vector<int> intersect(std::vector<int>& nums1, std::vector<int>& nums2){
     std::vector<int> Result;
-    std::unordered_set<int> HashSet;
-
     int size1 = nums1.size();
     int size2 = nums2.size();
 
@@ -13,45 +11,37 @@ std::vector<int> intersect(std::vector<int>& nums1, std::vector<int>& nums2){
     if (size1 > size2){
         bool seen = false;
         //This means Nums1 is larger.
-        for(int i =0; i < size2; i++){
-            for(int j =0; j<size1;j++){
+        for(int i =0; i < nums2.size(); i++){
+            for(int j =0; j<nums1.size();j++){
                 if(nums2[i] == nums1[j]){
                     //We have Found a match.
                     std::cout<<"We have Found a match\n";
-                    seen = true;
-                }
-            }
-            //No intersection Exists for this Value, Lets remove it from the vector and shift the values to right.
-            if(!seen && i != size2-1 ){
-                for(int it = i; it < size2; it++){
-                    nums2[it] = nums2[it+1];
+                    Result.push_back(nums2[i]);
+                    nums1.erase(nums1.begin() + j);
+                    break;
                 }
             }
         }
-
-    return nums2;
     }
     else{
         bool seen = false;
-        for(int i = 0; i<size1;i++){
-            for(int j=0; j<size2;j++){
+        for(int i = 0; i<nums1.size();i++){
+            for(int j=0; j<nums2.size();j++){
                 if(nums1[i] == nums2[j]){
                     //Found a Match.
                     std::cout<<"Found a Match\n";
-                }
-            }
-            if(!seen && i!=size1-1){
-                for(int it=i;it<size1;it++){
-                    nums1[it] = nums1[it+1];
+                    Result.push_back(nums1[i]);
+                    nums2.erase(nums2.begin() + j);
+                    break;
                 }
             }
         }
-    return nums1;
-    } 
+    }
+    return Result;
 }
 int main(){
-    std::vector<int> nums1 = {4,9,5};
-    std::vector<int> nums2 = {9,4,9,8,4};
+    std::vector<int> nums1 ={43,85,49,2,83,2,39,99,15,70,39,27,71,3,88,5,19,5,68,34,7,41,84,2,13,85,12,54,7,9,13,19,92};    
+    std::vector<int> nums2 = {10,8,53,63,58,83,26,10,58,3,61,56,55,38,81,29,69,55,86,23,91,44,9,98,41,48,41,16,42,72,6,4,2,81,42,84,4,13};
     //Expected Output = [2,2]; //Only Return the Intersections and however many times they appear.
         //Example
             //Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
