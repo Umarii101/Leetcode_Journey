27b59d27c84c49a57b349757d7f4c69e10b0412e
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-03 19:40:54 +0800

    Started Problem [12] Contains Duplicates II

---
+++ [2] Hash Tables/[12] Contains Duplicate II/Contains_DupII.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+
+bool containsNearbyDuplicate(vector<int>& nums, int k) {
+   
+    return false;
+}
+
+int main(){
+
+
+
+    return 0;
+}
