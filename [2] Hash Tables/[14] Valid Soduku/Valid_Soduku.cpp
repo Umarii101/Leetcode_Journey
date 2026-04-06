7ac60be7472f768bf64eafd98ca1e116223ced1f
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-06 19:53:00 +0800

    Built The Whole Logic and Test. Will Optimize it next

--- [2] Hash Tables/[14] Valid Soduku/Valid_Soduku.cpp
+++ [2] Hash Tables/[14] Valid Soduku/Valid_Soduku.cpp
@@ -25,24 +25,59 @@ bool isValidSudoku(std::vector<std::vector<char>>& board){
                     return false;
                 }
                 columns[c].insert(val);
-        }
 
+                //Need to create logic to detect 9 '3x3'SubBoxes in The board and keep track of which SubBox We are in Right now.
+                int current_SubBox;
+                if(r<=2 && c<=2){//Since R and C  initialized from 0.
+                    current_SubBox = 1;
+                }
+                if(r<=2 && c>=2 && c<=5){
+                    current_SubBox = 2;
+                }
+                if(r<=2 && c>=5 && c<=8){
+                    current_SubBox = 3;
+                }
+                if(r>2 && r<=5 && c<=2){
+                    current_SubBox = 4;
+                }
+                if(r>2 && r<=5 && c>2 && c<=5){
+                    current_SubBox = 5;
+                }
+                if(r>2 && r<=5 && c>5 && c<=8){
+                    current_SubBox = 6;
+                }
+                if(r>5 && r<=8 && c<=2){
+                    current_SubBox = 7;
+                }
+                if(r>5 && r<=8 && c>2 && c<=5){
+                    current_SubBox = 8;
+                }
+                if(r>5 && r<=8 && c>5 && c<=8){
+                    current_SubBox = 9;
+                }
+                if(subBoards[current_SubBox].count(val)>0){
+                    std::cout<<"The SubBoard Number : ["<<current_SubBox<<"] is not Valid.\n";
+                    return false;//Since this Value Already Exists in our SubBoard.
+                }
+                subBoards[current_SubBox].insert(val);//OtherWise Lets Add The Value To the SubBoard.
+        }
     }
-    
     return true;
 }
 
 int main(){
     std::vector<std::vector<char>> board = {
-         {'5','3','.','.','7','.','.','.','.'}
-        ,{'6','.','.','1','9','5','.','.','.'}
-        ,{'.','9','8','.','.','.','.','6','.'}
-        ,{'8','.','.','.','6','.','.','.','3'}
-        ,{'4','.','.','8','.','3','.','.','1'}
-        ,{'7','.','.','.','2','.','.','.','6'}
-        ,{'.','6','.','.','.','.','2','8','.'}
-        ,{'5','.','.','4','1','9','.','.','5'}
-        ,{'.','.','.','.','8','.','.','7','9'}
+         {'5','3','.',  '.','7','.',    '.','.','.'}
+        ,{'6','.','.',  '1','9','5',    '.','.','.'}
+        ,{'.','9','8',  '.','.','.',    '.','6','.'}
+
+        ,{'8','.','.',  '.','6','.',    '.','.','3'}
+        ,{'4','.','.',  '8','.','3',    '.','.','1'}
+        ,{'7','.','.',  '.','2','8',    '.','.','6'}
+
+        ,{'.','6','.',  '.','.','.',    '2','8','.'}
+        ,{'.','.','.',  '4','1','9',    '.','.','5'}
+        ,{'.','.','.',  '.','8','.',    '.','7','9'}
     };
     //Expected Output = True.
     bool Result = isValidSudoku(board);
