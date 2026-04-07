4667974d6d9074d8a923246328b37ded57051d98
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-07 19:47:10 +0800

    Started Problem [15] Find Duplicate SubTrees

---
+++ [2] Hash Tables/[15] Find Duplicate Subtrees/Dup_SubTrees.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+std::vector<Treenode*> findDuplicateSubTrees(Treenode* root){
+
+
+}
+
+int main(){
+    Treenode root = {2,2,2,3,null,3,null} 
+    //Output = [[2,3],[3]]
+    return 0;
+}
