ec26ddcf1c5297fb9019f0ffacd265ca1325f898
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-09 19:18:57 +0800

    Created The Binary Tree

--- [2] Hash Tables/[15] Find Duplicate Subtrees/Dup_SubTrees.cpp
+++ [2] Hash Tables/[15] Find Duplicate Subtrees/Dup_SubTrees.cpp
@@ -10,13 +10,34 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-std::vector<Treenode*> findDuplicateSubTrees(Treenode* root){
+std::vector<TreeNode*> findDuplicateSubTrees(TreeNode* root){
+    std::vector<TreeNode*> Result;
 
+    return Result;
 
 }
 
 int main(){
-    TreeNode root = {2,2,2,3,null,3,null} 
+                                                                TreeNode* root = new TreeNode(1);
+                                        TreeNode* n2 = new TreeNode(2);             TreeNode* n3 = new TreeNode(3);     
+         TreeNode* n4 = new TreeNode(4);                                        TreeNode* n5 = new TreeNode(2);  TreeNode* n7 = new TreeNode(4);   
+                                                                            TreeNode* n6 = new TreeNode(4);
+    
+
+    n3->right = n7;
+    n5->left = n6;
+    n3->left = n5;
+    n2->left = n4;
+    root->left = n2;
+    root->right = n3;
+
+    //The Tree has been Created. Lets Verify it.
+    std::cout<<root->left->left->val<<std::endl;
+
+    std::cout<<root->right->left->left->val<<std::endl;
+
+
+    //{2,2,2,3,null,3,null} 
     //Output = [[2,3],[3]]
     return 0;
 }
