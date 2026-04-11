5013757fcf9918c805270dfb904c617ef3bb6e10
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-11 20:37:01 +0800

    Bugs fixed using Sliding Window and Two pointers Approach. Code Accepted

--- [2] Hash Tables/[17] Longest Substring Without Repeating Characters/Longest_Substring_No_Repeat.cpp
+++ [2] Hash Tables/[17] Longest Substring Without Repeating Characters/Longest_Substring_No_Repeat.cpp
@@ -1,36 +1,32 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
-#include <vector>
+
 int lengthOfLongestSubstring(std::string s) {
-    int Current_Count = 0;
+    int left = 0;
     int longest_length = 0;
     std::unordered_set<char> validator;
-    for(int i = 0; i<s.length(); i++){
-        std::cout<<"The Current Character -> ["<<s[i]<<"]\n";
-        if(validator.count(s[i]) > 0){
-            //This Means that the character has been Repeated.
-            validator.clear();
-            validator.insert(s[i]);
-            if(Current_Count > longest_length){
-                longest_length = Current_Count;
-            }
-            Current_Count = 1;
-            std::cout<<"\tIter Num -> ["<<i<<"] If Block\tCurrent Count = "<<Current_Count<<"\tLongest Length Yet = "<<longest_length<<"\n";
+    for(int right = 0; right<s.length(); right++){
+        std::cout<<"The Current Character -> ["<<s[right]<<"]\n";
+        //This Means that the character has been Repeated.
+        while(validator.count(s[right])){
+            validator.erase(s[left]);
+            left++;
         }
-        else{
-            validator.insert(s[i]);
-            Current_Count++;
-            std::cout<<"\tIter Num -> ["<<s[i]<<"] Else Block\tCurrent Count = "<<Current_Count<<"\tLongest Length Yet = "<<longest_length<<"\n";
-        } 
-    }
+        //Add The Current Character.
+        validator.insert(s[right]);
+        int current_window_size = right - left + 1;
+        if(current_window_size > longest_length) {
+            longest_length = current_window_size;
+        }
+    } 
     return longest_length;
 }
 
 int main(){
     //std::string s = "abcabcbb";    //Expected Output = 3. "Since abc appears and are repeated but we count them atleast Once.
     //std::string s = "pwwkew";     //Expected Output = 3.
-    std::string s = "bbbbb";      //Expected Output = 1.
+    std::string s = "dvdf";      //Expected Output = 3.
     int Result = lengthOfLongestSubstring(s);
     std::cout<<"The Total Length of the Substring is ["<<Result<<"]\n";
     return 0;
