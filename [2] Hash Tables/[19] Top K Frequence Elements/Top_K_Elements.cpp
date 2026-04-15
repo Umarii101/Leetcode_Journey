1f75ab75798e9a6ed08b2772e36c5de21224b831
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-15 17:36:22 +0800

    Built Core Logic, Will Test with multiple Inputs next

--- [2] Hash Tables/[19] Top K Frequence Elements/Top_K_Elements.cpp
+++ [2] Hash Tables/[19] Top K Frequence Elements/Top_K_Elements.cpp
@@ -6,36 +6,25 @@
 std::vector<int> topKFrequent(std::vector<int>& nums, int k){
     std::vector<int> Result;
     std::unordered_map<int,int> freq;
-    std::unordered_set<int> filter;
+
     for(int num : nums){
-        if(freq[num].count()>0){
-            freq[num]++;//Update the count for that number if it already exists in the map. (meaning everytime a number is repeated, it's index is incremented.
-        }
-        else{
-            freq.insert(num);
-        }
+        freq[num]++; //Build the Freq Map First. Add all the Number from Num and their Frequency as well. 
     }
-    for(int j =0; j<k;j++){
-        int temp = 0;
-        for(int i=0; i<nums.size();i++){
-            filter.insert(freq[nums[i]]);
-            if(i==0){
-                continue;
-            }
-            if(i>0){
-                if(filter[nums[i]]>filter[nums[i-1]]){
-                    filter.erase(nums[i-1]);
-                    temp = nums[i];
-                    freq.erase(nums[i]);//Delete The Biggest k we found so the next K-Loop will not have this big value and will find the next big K.
-                }
-                else{
-                    filter.erase(nums[i]);
-                    temp = nums[i-1];
-                    freq.erase(nums[i-1]);//Delete The Biggest k we found so the next K-Loop will not have this big value and will find the next big K.
-                }
-            }
-        }//At the end of this loop we shall have the Biggest of the K. Now we need k-1 More Entries in the Result Vector.
-         Result.push_back(temp);    //Now we need to store Top K in the Result Vector.
+
+    for(int i = 0; i<k; i++){
+        int maxfreq = -1;// We are to initialize this as negative, since All the elements atleast appear 1 times. Never negative. This is for safety only.
+        int mostFreqNumb = 0;
+        
+        for(auto it = freq.begin(); it != freq.end();++it){
+            maxfreq = it -> second;
+            mostFreqNumb = it -> first;
+        }
+
+        //Now at This point we have found the Biggest Winnder "K". Lets add it to our Result.
+        //Also delete it from Freq map so it does not win next round.
+
+        Result.push_back(mostFreqNumb);
+        freq.erase(mostFreqNumb);
     }
     return Result;
 }
