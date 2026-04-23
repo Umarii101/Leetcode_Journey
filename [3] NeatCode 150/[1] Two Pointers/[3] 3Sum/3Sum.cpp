c3329b42776e038b981381bec87012b9e0775ffc
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-23 17:46:58 +0800

    Started Problem [3] 3Sum

---
+++ [3] NeatCode 150/[1] Two Pointers/[3] 3Sum/3Sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> threeSum(std::vector<int>& nums){
+    std::vector<std::vector<int>> Result;
+
+
+
+    return Result;
+}
+
+int main(){
+    std::vector<int> nums ={-1,0,1,2,-1,-4};
+    std::vector<std::vector<int> Result = threeSum(nums);
+    //Expected Output = [[-1,-1,2],[-1,0,1]] //It returns as A vector that contains Vectors that add up to Zero.
+
+    return 0;
+}
