52fb8a8ec0bd66cfe72a5662f027104e2c98bcff
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-24 17:27:17 +0800

    Started Problem [4] Container with most Water

---
+++ [3] NeatCode 150/[1] Two Pointers/[4] Container with most Water/Cont_WmstWtr.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int maxArea(std::vector<int>& heights){
+    int Result;
+
+    return Result;
+}
+
+int main(){
+    std::vector<int> heights = {1,7,2,5,4,7,3,6};
+    int Result = maxArea(heights);
+    //Expected Output = 36 
+    std::cout<<"The Result is : "<<Result<<".\n";
+    return 0;
+}
