f10e77799488f7119455f422fa7c7e679b57b53c
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-25 16:48:44 +0800

    Started Problem [5] Trapping Rain Water

---
+++ [3] NeatCode 150/[1] Two Pointers/[5] Trapping Rain Water/Trap_Rain_Wtr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int trap(std::vector<int>& height) {
+    int Result;
+
+    return Result;
+}
+
+int main(){
+    std::vector<int> height = {4,2,0,3,2,5};
+    int result = trap(height);
+    std::cout<<"The Result is "<<result<<"\n";
+    //Output =  9
+
+    return 0;
+}
