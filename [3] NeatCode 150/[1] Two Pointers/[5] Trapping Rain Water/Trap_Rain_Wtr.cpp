d271645f66fe8f5744aa6c99d61ab814f2b705d5
Muhammad Umar <sultanumar2002@gmail.com>
2026-04-26 17:04:10 +0800

    Building Core Logic, Implementing BarTotalArea Calculation Technique

--- [3] NeatCode 150/[1] Two Pointers/[5] Trapping Rain Water/Trap_Rain_Wtr.cpp
+++ [3] NeatCode 150/[1] Two Pointers/[5] Trapping Rain Water/Trap_Rain_Wtr.cpp
@@ -3,6 +3,25 @@
 
 int trap(std::vector<int>& height) {
     int Result;
+    //First  Things i noticed is that The Bars have Area aswell this time, and the problem is pretty much the same as previous one.
+    //Lets create a bar area calculation first, then we will trap water between the bars and return the total amount of water trapped between the bars.
+
+    for(int i = 0; i<heights.size()-1; i++){
+        int currBarArea = 0;
+        if(heights[i] != 0){
+            while(i != heights.size()-2 && heights[i+1] != 0){
+                currBarArea = currBarArea + heights[i] + heights[i+1];
+                i++;
+            }
+        }
+
+
+
+    }
+
+
+
+
 
     return Result;
 }
